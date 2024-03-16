477d585989bcb83797069f8b848d6f609c3b53f8
Sara Justino <sara.sjustino@gmail.com>
2024-03-16 16:11:58 -0300

    Atualização Sara

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CadastroController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('cadastro',[CadastroController::class,'cadastro'])->name('cadastro.index');
